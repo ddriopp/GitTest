3144cf4efafb774735a93bd9cf26a66c4937ff4c
ddriopp <ddriopp@163.com>
2018-09-07 00:49:30 +0800

    added GankouNoodles

---
+++ src/com/ddriop/git/GankouNoodles.java
@@ -0,0 +1,11 @@
+package com.ddriop.git;
+
+public class GankouNoodles extends INoodles {
+
+	@Override
+	public void desc() {
+		// TODO Auto-generated method stub
+		System.out.println("good");
+	}
+
+}

833754a492a051fbd5eb3490ebe5b98a5f8cae83
ddriopp <ddriopp@163.com>
2018-09-07 00:58:18 +0800

    fix issues

--- src/com/ddriop/git/MyMessageFactory.java
+++ src/com/ddriop/git/MyMessageFactory.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MyMessageFactory implements IMyMessageFactory {
-	
+
 	@Override
 	public IMyMessage createMessage(String messageType) {
 		IMyMessage myMessage;
@@ -12,8 +12,17 @@ public class MyMessageFactory implements IMyMessageFactory {
 		if ("SMS".equals(messageType)) {
 			myMessage = new MyMessageSms();
 			messageParam.put("PHONENUM", "123456789");
+		} else if ("OA".equals(messageType)) {
+			myMessage = new MyMessageOaTodo();
+			messageParam.put("OAUSERNAME", "testUser");
+		} else if ("EMAIL".equals(messageType)) {
+			myMessage = new MyMessageEmail();
+			messageParam.put("EMAIL", "test@test.com");
+		} else {
+			myMessage = new MyMessageEmail();
+			messageParam.put("EMAIL", "test@test.com");
 		}
-
+		myMessage.setMessageParam(messageParam);
 		return myMessage;
 	}
 }

1beb124f1e3281fe4182685d46e251221586fbbf
ddriopp <ddriopp@163.com>
2018-09-07 00:26:54 +0800

    add PaoNoodles

---
+++ src/com/ddriop/git/PaoNoodles.java
@@ -0,0 +1,11 @@
+package com.ddriop.git;
+
+public class PaoNoodles extends INoodles {
+
+	@Override
+	public void desc() {
+		// TODO Auto-generated method stub
+		System.out.println("nice!");
+	}
+
+}
